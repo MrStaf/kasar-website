3583cf3d9242effa3a7efa21beeb23da7f192384
mrstaf <ben82160@gmail.com>
2021-07-30 16:28:29 +0200

    Add NProgress bar

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,24 @@
 import "../styles/globals.css";
 import "../styles/card.css";
 import "../styles/links.css";
+import { Router } from "next/dist/client/router";
+import NProgress from "nprogress";
+import "../styles/nprogress.css";
 
-function MyApp({ Component, pageProps }) {
+NProgress.configure({ showSpinner: false });
+
+Router.events.on("routeChangeStart", () => {
+  NProgress.start();
+});
+Router.events.on("routeChangeComplete", () => {
+  NProgress.done();
+});
+Router.events.on("routeChangeError", () => {
+  NProgress.remove();
+});
+
+function Kasar({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
 
-export default MyApp;
+export default Kasar;
