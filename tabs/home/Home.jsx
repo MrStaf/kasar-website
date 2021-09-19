0c4a38170dc0677722cb3949baa6f507d56984fc
MrStaf <ben82160@gmail.com>
2021-09-19 19:09:54 +0200

    Update content and zindex

--- tabs/home/Home.jsx
+++ tabs/home/Home.jsx
@@ -2,6 +2,9 @@
 import Head from "next/head";
 import Image from "next/image";
 
+// Components
+import { Title } from "../../components/Title/";
+
 // SVG
 import RobotKasar from "../../img/robot.svg";
 import BgHero from "../../img/bg-hero.jpg";
@@ -18,19 +21,28 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="flex flex-col items-center flex-grow w-full overflow-y-scroll font-body">
-        <div className="flex flex-col justify-center w-full max-w-5xl">
+        <div className="flex flex-col justify-center w-full max-w-5xl mb-4">
           <div className="grid">
             <h1 className="flex flex-col items-center mt-10">
-              <span className="z-50 text-4xl font-logo-light">
+              <span className="text-4xl font-logo-light">
                 Bienvenue sur
               </span>
-              <span className="z-50 text-transparent text-7xl font-logo bg-clip-text bg-gradient-to-r from-primary to-secondary">
+              <span className="text-transparent text-7xl font-logo bg-clip-text bg-gradient-to-r from-primary to-secondary">
                 Kasar
               </span>
             </h1>
             <Image src={RobotKasar} alt="Robot kasar" />
           </div>
-          <Image src={BgHero} alt="La team kasar 2021-2022" />
+          <div className="px-8 mb-4">
+            <Title>Notre mission</Title>
+            <p className="text-justify">
+              <span className="font-bold text-primary">Kasar</span> est une association de l&apos;ECE Paris, qui a pour but de
+              démystifier l’intelligence artificielle, et de former une
+              communauté d&apos;entraide et de partage autour du machine
+              learning.
+            </p>
+          </div>
+          <Image src={BgHero} alt="La team kasar 2021-2022"/>
         </div>
       </main>
     </>
