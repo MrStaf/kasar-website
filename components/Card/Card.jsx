ba8afdbfd768616872d2968577fa8740439b6238
MrStaf <ben82160@gmail.com>
2021-09-19 18:41:59 +0200

    alt for images

--- components/Card/Card.jsx
+++ components/Card/Card.jsx
@@ -14,7 +14,7 @@ export default function Card({ title, subtitle, id, completed = false, img, clas
     <div className={`${className !== undefined ? className : ""} flex justify-center card`}>
       <Link href={{ pathname: pathname, query: query }} passHref={passHref}>
         <a className="flex flex-col items-center text-xl font-text w-72">
-          <div className="relative flex items-center justify-center cursor-pointer h-72 w-72 bg-secondary rounded-xl">{img && <Image src={ASSETS_URL + img} layout="fill" objectFit="cover" className="rounded-xl" />}</div>
+          <div className="relative flex items-center justify-center cursor-pointer h-72 w-72 bg-secondary rounded-xl">{img && <Image src={ASSETS_URL + img} alt="Descriptive image for an event" layout="fill" objectFit="cover" className="rounded-xl" />}</div>
           <div className="w-full mt-2 font-semibold">{title}</div>
           <div className={`${completed ? "line-through" : ""} w-full font-body`}>{subtitle}</div>
         </a>
